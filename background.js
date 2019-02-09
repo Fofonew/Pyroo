ed4cd4584e0f5b3940c2e55fd71928c2c942640b
Dimitri ORIOL <doriol@student.42.fr>
2019-02-09 05:21:57 +0100

    GIT VERSION

--- background.js
+++ background.js
@@ -1,8 +1,8 @@
 var bool = 1;
 var options = {
   type: "basic",
-  title: "PyrooTv Extension",
-  message: "Uh oh! PyrooTv est en live !",
+  title: "*** Extension",
+  message: "Uh oh! *** est en live !",
   iconUrl: "images/logo.png"
 };
 var myAudio = new Audio(chrome.runtime.getURL("images/sound.mp3"));
@@ -16,16 +16,13 @@ setInterval(function(){
 
 function update(){
   var xhr = new XMLHttpRequest();
-  xhr.open("GET", "https://api.twitch.tv/kraken/streams/pyrootv?client_id=y5bbcba0yl7by0oar7ynn2b6og3u87", true);
+  xhr.open("GET", "https://api.twitch.tv/kraken/streams/***?client_id=***", true);
   xhr.onreadystatechange = function(channel) {
     if(xhr.readyState == 4) {
       var data = JSON.parse(xhr.responseText);
       if(data["stream"] == null){
         bool = 1;
         chrome.browserAction.setBadgeText({text: ""});
-        //$("#info").html("<font color='red'><strong>PyrooTv est actuellement offline.</strong></font>");
-        //$("#follow").html("0");
-        //$("#view").html("0");
       }
       else{
         if (bool == 1) {
@@ -33,16 +30,6 @@ function update(){
           myAudio.play();
           bool = 2;
         }
-        //var title = "&#9679 " + data["stream"]["channel"]["status"];
-        //var game = "&#9679 " + data["stream"]["game"];
-        //$("#game").html(game);
-        //$("#title").html(title);
-        //var img_out = '<a href="https://twitch.tv/pyrootv" target="_blank"><img src="' + data["stream"]["preview"]["medium"] + '" class="images_screen" alt="Stream" title="Stream"/></a>';
-        //var follow = data["stream"]["channel"]["followers"];
-        //$("#follow").html(follow);
-        //var view = data["stream"]["viewers"];
-        //$("#view").html(view);
-        //$("#info").html(img_out);
         chrome.browserAction.setBadgeText({text: "ON"});
         chrome.browserAction.setBadgeBackgroundColor({color: "green"});
       }
--- update.js
+++ update.js
@@ -1,12 +1,12 @@
 var xhr = new XMLHttpRequest();
-xhr.open("GET", "https://api.twitch.tv/kraken/streams/pyrootv?client_id=y5bbcba0yl7by0oar7ynn2b6og3u87", true);
+xhr.open("GET", "https://api.twitch.tv/kraken/streams/***?client_id=***", true);
 xhr.onreadystatechange = function(channel) {
   if(xhr.readyState == 4) {
     var data = JSON.parse(xhr.responseText);
 
     if(data["stream"] == null){
       chrome.browserAction.setBadgeText({text: ""});
-      $("#info").html("<font color='red'><strong>PyrooTv est actuellement offline.</strong></font>");
+      $("#info").html("<font color='red'><strong>*** est actuellement offline.</strong></font>");
       $("#follow").html("0");
       $("#view").html("0");
     }
@@ -15,7 +15,7 @@ xhr.onreadystatechange = function(channel) {
       var game = "&#9679 " + data["stream"]["game"];
       $("#game").html(game);
       $("#title").html(title);
-      var img_out = '<a href="https://twitch.tv/pyrootv" target="_blank"><img src="' + data["stream"]["preview"]["medium"] + '" class="images_screen" alt="Stream" title="Stream"/></a>';
+      var img_out = '<a href="https://twitch.tv/***" target="_blank"><img src="' + data["stream"]["preview"]["medium"] + '" class="images_screen" alt="Stream" title="Stream"/></a>';
       var follow = data["stream"]["channel"]["followers"];
       $("#follow").html(follow);
       var view = data["stream"]["viewers"];
